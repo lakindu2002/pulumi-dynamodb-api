642efe7e625e6386bf516b7675dbcbee33e8f582
lakindu hewawasam <lakindu2002@live.com>
2022-06-05 21:34:03 +0530

    exporting the API Gateway invoke URL

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 // stack resources are declared in this file
 import { Users } from './dynamo/users';
+import { apiGateway } from './api-gateway/index';
 
 export const UsersTable = Users.name;
+export const { url } = apiGateway;
